79827473877f388e41054f406b8344fb0bd8169e
Gayathri <70088536+GAYATHRI-K-M@users.noreply.github.com>
2021-04-21 20:01:04 +0530

    Add files via upload

---
+++ positive num.py
@@ -0,0 +1,15 @@
+# to print positive numbers
+
+list1 = [12,-7,5,64,-14]
+
+for num in list1:
+    if num >= 0:
+        print(num, end = " ")
+        
+        
+list2 = [5,34,-95,3]
+
+for num in list2:
+    if num >= 0:
+        print(num, end = " ")
+        
