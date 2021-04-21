fe83379db2bb7e3d25fececb8fa953fde64661a8
Gayathri <70088536+GAYATHRI-K-M@users.noreply.github.com>
2021-04-21 19:30:28 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+#fibonacci numbers
+
+nterms = int(input("enter the number of terms: "))
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+   print("invalid input")
+elif nterms == 1:
+     print("Fibonacci series upto" ,nterms,": ")
+     print(n1)
+else:
+    print("fibonacci series: ")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count =+ 1 
+        
